f33c77ab60964129d96f9d19c7032e20398e8378
Tarun222999 <tarun@levitation.co.in>
2025-12-13 20:17:51 +0530

    Generate speech using tts-1

---
+++ app/api/generate-speech/route.ts
@@ -0,0 +1,25 @@
+import { openai } from "@ai-sdk/openai";
+import { experimental_generateSpeech as generateSpeech } from "ai";
+
+
+export async function POST(req: Request) {
+    try {
+
+        const { text } = await req.json()
+
+        const { audio } = await generateSpeech({
+            model: openai.speech("tts-1"),
+            text
+        })
+
+
+        return new Response(new Uint8Array(audio.uint8Array), {
+            headers: {
+                "Content-Type": audio.mediaType || "audio/mpeg",
+            },
+        });
+    } catch (error) {
+        console.log("Error generating image:", error)
+        return new Response("Failed to generate image", { status: 500 })
+    }
+}
